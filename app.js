ba9acb23ef874b56d93104b48ae5e43b8a562c62
Alex10liva <alex_oliv1000@hotmail.com>
2022-11-19 15:44:57 -0600

    fixed label errors and functional pop ups added

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-// Home page functions
-function toggleLogIn() {
-    let popup = document.getElementById("form-bg-log-in");
-    if (popup.classList.contains("active")) {
-        popup.classList.remove("active");
-    } else {
-        popup.classList.add("active");
-    }
-}
-
-function toggleSignUp() {
-    let popup = document.getElementById("form-bg-sign-up");
+// Main functions
+function togglePopUp(popupID) {
+    console.log("Se apreto")
+    let popup = document.getElementById(popupID);
     if (popup.classList.contains("active")) {
         popup.classList.remove("active");
     } else {
@@ -18,21 +10,28 @@ function toggleSignUp() {
 }
 
 // Cazador page functions
-let toggle = document.querySelector('.toggle');
-let navigation = document.querySelector('.navigation');
-let main = document.querySelector('.main');
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
 
 toggle.onclick = function() {
-    navigation.classList.toggle('active');
-    main.classList.toggle('active');
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
 }
 
-let list = document.querySelectorAll('.navigation li');
+let list = document.querySelectorAll(".navigation li");
 
 function activeLink() {
     list.forEach((item) =>
-        item.classList.remove('hovered'));
-    this.classList.add('hovered');
+        item.classList.remove("hovered"));
+    this.classList.add("hovered");
 }
-list.forEach((item) =>
-    item.addEventListener('mouseover', activeLink));
+
+// Talento page functtions
+function showOptions() {
+    document.getElementById("propose-new").style.display = "block";
+}
+
+function hideOptions() {
+    document.getElementById("propose-new").style.display = "none";
+}
